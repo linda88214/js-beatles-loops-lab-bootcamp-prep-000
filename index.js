724f25a1affa4de7f91919d90d9a99ce3c367f4c
Yerim Hu <yerimhu88@gmail.com>
2017-10-24 02:28:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 function theBeatlesPlay(musicians, instruments) {
-<<<<<<< HEAD
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`);
@@ -7,29 +6,4 @@ function theBeatlesPlay(musicians, instruments) {
   return array
 }
 
-function johnLennonFacts(array) {
-  var i = 0;
-  while (i < array.length) {
-    array[i] = array[i] + "!!!";
-    i++
-  }
-  return array
-}
 
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    array.push('I love the Beatles!');
-    number++;
-  } while (number < 15);
-  return array
-}
-=======
-  var array = {}
-  for (var i = 0; i < musicians.length; i++) {
-    var string = `${musicians[i]} plays ${instruments[i]}`;
-    array.push(string);
-  }
-  return array
-}
->>>>>>> cc900ee1129c25193f8b2773e06efdceb4db85d9
